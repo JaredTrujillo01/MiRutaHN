7bd93d5e581c419c053fc8d1caec45b8e6840694
Eduardo Jared Trujillo Gamero <167134639+JaredTrujillo01@users.noreply.github.com>
2026-05-05 09:31:59 -0600

    Estructura de dashboard
    Se realizo la estructura del dashboard de como debe ir funcionando

---
+++ src/app/components/resultados-busqueda/resultados-busqueda.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-resultados-busqueda',
+  imports: [CommonModule],
+  templateUrl: './resultados-busqueda.html',
+  styleUrl: './resultados-busqueda.scss',
+})
+export class ResultadosBusqueda {
+  @Output() verDetalle = new EventEmitter<any>();
+  @Output() volver = new EventEmitter<void>();
+
+  rutas = [
+    { id: 1, nombre: 'Ruta 2 - Anillo Periférico', precio: 13, tiempo: 45, llega: '10 min', active: true },
+    { id: 2, nombre: 'Ruta 35 - Centro Directo', precio: 16, tiempo: 30, llega: '18 min', active: false },
+    { id: 3, nombre: 'Trans-H - Rápido Metropolitano', precio: 20, tiempo: 22, llega: '2 min', active: false }
+  ];
+
+  seleccionarRuta(ruta: any) {
+    this.verDetalle.emit(ruta);
+  }
+}
