3fed72056f4847c2d22b1936b1fd27b6cab2b988
Eduardo Jared Trujillo Gamero <167134639+JaredTrujillo01@users.noreply.github.com>
2026-05-09 18:14:00 -0600

    Configuracion con firebase
    Se configuro la api Key de firebase para poder empezar a usar la base de datos de esta misma

--- src/app/pages/publico/login/login.ts
+++ src/app/pages/publico/login/login.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
