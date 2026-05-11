7dcf8f964ff7dcaaa8038bffdc0650eef3ca283d
Fany Navas <fanynavas@unitec.edu>
2026-05-11 17:55:39 -0600

    Configuracion FN de Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { initializeApp } from 'firebase/app';
+
 import { routes } from './app.routes';
+import { firebaseConfig } from './firebase.config';
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 export const appConfig: ApplicationConfig = {
   providers: [
