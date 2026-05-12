d11da77ab68a59ea91c3249806499e8c3d776616
Fany Navas <fanynavas@unitec.edu>
2026-05-12 14:11:02 -0600

    7-firebase-auth-login

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, doc, setDoc } from 'firebase/firestore';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { getDoc } from 'firebase/firestore';
 
 export interface RegistroUsuario {
   nombre: string;
@@ -16,6 +19,28 @@ export interface RegistroUsuario {
 export class AuthService {
   private auth = getAuth();
   private db = getFirestore();
+
+  loginUsuario(email: string, password: string) {
+  return signInWithEmailAndPassword(this.auth, email, password);
+   }
+
+  obtenerUsuarioActual(): Promise<User | null> {
+    return new Promise((resolve) => {
+      onAuthStateChanged(this.auth, (user) => {
+        resolve(user);
+      });
+    });
+  }
+
+  async obtenerPerfilUsuario(uid: string) {
+    const documento = await getDoc(doc(this.db, 'usuarios', uid));
+
+    if (!documento.exists()) {
+      return null;
+    }
+
+    return documento.data();
+  }
 
   async registrarUsuario(usuario: RegistroUsuario) {
     const credenciales = await createUserWithEmailAndPassword(
